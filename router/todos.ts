b3580b04013edb8d85bd2785bbda7518305f51b0
Esteban Barragan Muñoz <esteban@akaunt.io>
2024-04-07 22:23:47 -0700

    refactor: add casting to leverage typescript features

--- router/todos.ts
+++ router/todos.ts
@@ -1,6 +1,11 @@
 import { Router, Request, Response } from "express";
 import Todo from "../models/todo";
+
+type RequestBody = { name: string }
+type RequestParams = { todoId: string }
+
 const router = Router();
+
 let todosList: Todo[] = []
 
 router.get('/', (req: Request, res: Response, next) => {
@@ -8,8 +13,9 @@ router.get('/', (req: Request, res: Response, next) => {
 })
 
 router.post('/create', (req, res, next) => {
+    const body = req.body as RequestBody
     const newTodo: Todo = {
-        name: req.body.name,
+        name: body.name,
         id: new Date().toISOString()
     }
     todosList.push(newTodo)
@@ -22,12 +28,13 @@ router.post('/create', (req, res, next) => {
 })
 
 router.put('/update/:todoId', (req, res) => {
-    const todoId = req.params.todoId
-    const todoIndex = todosList.findIndex(todoItem => todoItem.id === todoId)
+    const body = req.body as RequestBody
+    const params = req.params as RequestParams
+    const todoIndex = todosList.findIndex(todoItem => todoItem.id === params.todoId)
     if (todoIndex >= 0) {
         todosList[todoIndex] = {
             id: todosList[todoIndex].id,
-            name: req.body.name,
+            name: body.name,
         }
         return res
             .status(200)
@@ -44,8 +51,8 @@ router.put('/update/:todoId', (req, res) => {
 })
 
 router.delete('/delete/:todoId', (req, res) => {
-    const todoId = req.params.todoId
-    todosList = todosList.filter(todoItem => todoItem.id !== todoId)
+    const params = req.params as RequestParams
+    todosList = todosList.filter(todoItem => todoItem.id !== params.todoId)
     return res
         .status(200)
         .json({
